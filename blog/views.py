447820f9a9fad5ed08a3b876b2ee71cd91c52d80
vickyr23 <vickyrsantay23@gmail.com>
2018-11-03 16:26:42 -0600

    Listar datos en miblog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def listado(request):
+    publicados = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar.html', {publicados:'publicados'})
 # Create your views here.
