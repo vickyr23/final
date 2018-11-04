fa8c29e0994f04bc891e6161d9b2b73cf9fa18b1
vickyr23 <vickyrsantay23@gmail.com>
2018-11-04 06:39:22 -0600

    editar y nuevo agregados exitosamente

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.listado),
     url(r'^detalle/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
+    url(r'^post/nuevo/$', views.nuevo, name='nuevo'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.editar, name='editar'),
+    url(r'^borrador/$', views.borrador, name='borrador'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,35 @@ def listado(request):
 def detalle(request, pk):
     det = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/detalle.html', {'det': det})
+
+def nuevo(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                p = form.save(commit=False)
+                p.autor = request.user
+                p.save()
+                return redirect('detalle', pk=p.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/editar.html', {'form': form})
+
+def editar(request, pk):
+        post = get_object_or_404(Publicacion, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('detalle', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/editar.html', {'form': form})
+
+def borrador(request):
+    draft = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/borrador.html', {'draft': draft})
 # Create your views here.
 
 def publish(self):
