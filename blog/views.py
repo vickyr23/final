af39eeeff4772c8a4fb79a4633538a684478d2af
vickyr23 <vickyrsantay23@gmail.com>
2018-11-04 15:39:35 -0600

    agregar login

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Publicacion
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def listado(request):
     publicados = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -12,6 +13,7 @@ def detalle(request, pk):
     det = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/detalle.html', {'det': det})
 
+@login_required
 def nuevo(request):
         if request.method == "POST":
             form = PostForm(request.POST)
@@ -24,6 +26,7 @@ def nuevo(request):
             form = PostForm()
         return render(request, 'blog/editar.html', {'form': form})
 
+@login_required
 def editar(request, pk):
         post = get_object_or_404(Publicacion, pk=pk)
         if request.method == "POST":
@@ -37,10 +40,12 @@ def editar(request, pk):
             form = PostForm(instance=post)
         return render(request, 'blog/editar.html', {'form': form})
 
+@login_required
 def borrador(request):
     draft = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
     return render(request, 'blog/borrador.html', {'draft': draft})
 # Create your views here.
+@login_required
 def publicacion(request, pk):
     pu = get_object_or_404(Publicacion, pk=pk)
     pu.publish()
@@ -50,6 +55,7 @@ def publish(self):
     self.fecha_publicacion = timezone.now()
     self.save()
 
+@login_required
 def eliminar(request, pk):
     rv = get_object_or_404(Publicacion, pk=pk)
     rv.delete()
--- misitio/urls.py
+++ misitio/urls.py
@@ -20,5 +20,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
     url(r'',include('blog.urls')),
 ]
