e38bf6a3a1a50a0fcb5bccaedb93d9bf005860c4
vickyr23 <vickyrsantay23@gmail.com>
2018-11-03 18:56:41 -0600

    agregar detalle publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listado),
+    url(r'^detalle/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,8 @@ from .models import Publicacion
 def listado(request):
     publicados = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar.html', {publicados:'publicados'})
+
+def detalle(request, pk):
+    det = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle.html', {'det': det})
 # Create your views here.
